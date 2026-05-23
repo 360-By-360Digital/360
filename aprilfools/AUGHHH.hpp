0be6fa307bd516b48f0756e9aecdf9ed563795e8
Zakii <zakii@360-search.com>
2026-05-23 13:57:07 -0400

    Add AUGHHH.hpp with basic C++ program

---
+++ aprilfools/AUGHHH.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Hello, world of hard coding!";
+    
+    
+    int age = 25;
+    std::cout << "Years spent learning C++ divided by age: " << age << std::endl; 
+    
+    return 0;
+}
